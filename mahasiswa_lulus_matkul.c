eb6564bde6422f1ddc0f0f1c1108ad4f3a7a51f8
Maulana Adam Sahid <masadamsahid@gmail.com>
2023-06-10 05:19:23 +0700

    Add looping inputs for `arrMhs` name values and print them.

---
+++ mahasiswa_lulus_matkul.c
@@ -0,0 +1,33 @@
+//
+// Created by masad on 6/10/2023.
+//
+
+#include <stdio.h>
+
+struct Mahasiswa {
+  char nama[127];
+  float nilai;
+};
+
+int main(){
+  
+  struct Mahasiswa arrMhs[100];
+  int lengthOfArrMhs = sizeof(arrMhs)/sizeof(arrMhs[0]);
+  
+  for (int i = 0; i < lengthOfArrMhs; i++) {
+    printf("Masukkan nama mhs ke-%d", i+1);
+    scanf("%[^\n]s", arrMhs[i].nama);
+    printf("\n");
+    fflush(stdin);
+  }
+  
+  for (int i = 0; i < lengthOfArrMhs; i++) {
+    printf("Nama mhs ke-%d: %s\n", i+1, arrMhs[i].nama);
+  }
+  
+  printf("%d\n", lengthOfArrMhs);
+  
+  getchar();
+  
+  return 0;
+}
